dce2838d89d3ac1c425614105147fdf7f8e7b8cf
Doris Lin <lmh7026@gmail.com>
2023-08-01 10:28:02 -0400

    build out wishcard

--- src/components/WishCard.jsx
+++ src/components/WishCard.jsx
@@ -1,8 +1,20 @@
+import './WishCard.css'
+
 const WishCard = ({wishData}) => {
     console.log('in WishCard', wishData)
     return(
-        <section>
-            {wishData.restaurant_name}
+        <section className='wish-card__container'>
+            <h4>Wish {wishData.wish_id}</h4>
+            <ul>
+                <li>Restaurant: {wishData.restaurant_name}</li>
+                <li>Priority: {wishData.wish_priority}</li>
+                <li>Comment: 
+                        {wishData.wish_comment}
+                </li>
+
+            </ul>
+
+            
         </section>
     )
 };
