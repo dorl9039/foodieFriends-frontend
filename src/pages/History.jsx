2a2d069354e65407be7f4a83a085691da9f05c45
Doris Lin <lmh7026@gmail.com>
2023-08-02 22:01:53 -0400

    implement useEffect for getting historyData

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,14 +1,22 @@
 import { useEffect, useState } from 'react';
 import VisitList from "../components/History/VisitList";
 import VisitDetails from "../components/History/VisitCard";
+import axios from 'axios';
 
 const History = ({ userId }) => {
     const [historyData, setHistoryData] = useState([]);
 
     useEffect(() => {
-        
+        axios
+        .get(`http://localhost:5000/users/${userId}/history`)
+        .then(res => {
+            setHistoryData(res.data)
+        })
+        .catch(err => {
+            console.log("Error in useEffect get history", err)
+        })
     }, [])
-    
+
     return (
         <div>
             <h2>History</h2>
