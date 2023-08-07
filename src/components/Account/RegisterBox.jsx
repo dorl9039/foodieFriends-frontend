8fb2dde6dbb9272096234e007315f522441f7653
Doris Lin <lmh7026@gmail.com>
2023-08-07 11:11:04 -0400

    remove StyledBox code, imported as module

--- src/components/Account/RegisterBox.jsx
+++ src/components/Account/RegisterBox.jsx
@@ -1,29 +1,7 @@
 import {useState, forwardRef} from 'react';
 import Modal from '@mui/base/Modal';
-import { styled } from '@mui/system';
-import clsx from 'clsx';
+import StyledBackdrop from '../StyledBackdrop';
 
-
-const Backdrop = forwardRef((props, ref) => {
-	const { open, className, ...other } = props;
-	return (
-		<div
-			className={clsx({ 'MuiBackdrop-open': open }, className)}
-			ref={ref}
-			{...other}
-		/>
-	);
-});
-
-Backdrop.displayName = 'Backdrop';
-
-const StyledBackdrop = styled(Backdrop)`
-	z-index: -1;
-	position: fixed;
-	inset: 0;
-	background-color: rgb(0 0 0 / 0.5);
-	-webkit-tap-highlight-color: transparent;
-	`;
 
 const kFormInitialState = {
 	username: '',
