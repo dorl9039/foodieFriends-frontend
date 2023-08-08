6731f8b93eb2468c6f529087fc502afccc6d2928
Doris Lin <lmh7026@gmail.com>
2023-08-08 18:45:50 -0400

    sort friend recs by user's wish priorities

--- src/components/Home/Rec.jsx
+++ src/components/Home/Rec.jsx
@@ -3,7 +3,7 @@ import './Rec.css'
 const Rec = ({recData}) => {
 	return (
 		<div className='rec__container'>
-			{recData.friend.first_name} ({recData.friend.username}) also wants to try out {recData.wishRestaurant.restaurant_name}
+			{recData.friend.firstName} ({recData.friend.username}) also wants to try out {recData.wish.restaurantName}
 		</div>
 	)
 }
