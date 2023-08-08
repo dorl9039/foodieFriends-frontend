67ce419e4a85d56bbc472f301192168cd052a3dc
Doris Lin <lmh7026@gmail.com>
2023-08-07 23:07:21 -0400

    insert map with hardcoded marker

--- src/components/WishlistPage/WishlistMap.jsx
+++ src/components/WishlistPage/WishlistMap.jsx
@@ -1,9 +1,22 @@
-const WishlistMap = () => {
-    return (
-        <>
-        <p>Map goes here</p>
-        </>
-    )
-};
+import * as React from 'react';
+import Map, {Marker} from 'react-map-gl';
+
+const WishlistMap = () =>{
+  return (
+    <Map
+      mapboxAccessToken={import.meta.env.VITE_MAPBOX_TOKEN}
+      initialViewState={{
+        longitude: -122.4,
+        latitude: 37.8,
+        zoom: 14
+      }}
+      style={{width: 600, height: 400}}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      >
+    <Marker longitude={-122.4104} latitude={37.8100} anchor="bottom" >
+    </Marker>
+    </Map>
+  );
+}
 
 export default WishlistMap;
