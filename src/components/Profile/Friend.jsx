629306a679d99717edf40386d8f94a454c60ed91
Doris Lin <lmh7026@gmail.com>
2023-08-13 11:46:44 -0400

    create modal for confirm remove friend

--- src/components/Profile/Friend.jsx
+++ src/components/Profile/Friend.jsx
@@ -1,17 +1,37 @@
+import { useState } from 'react';
+import Modal from '@mui/base/Modal';
+import StyledBackdrop from '../StyledBackdrop';
+
 import './Friend.css'
 
 const Friend = ({friendData, handleRemove}) => {
+  const [deleteOpen, setDeleteOpen] = useState(false)
+  const onConfirmDelete = () => {
+		setDeleteOpen(false)
+		handleRemove(friendData.userId)
+	}
   
-  const onRemove = () => {
-    handleRemove(friendData.userId)
-  }
 
   return (
     <div className='friend-item__container'>
       <p>{friendData.username}</p>
       <p>{friendData.firstName}</p>
       <p>{friendData.lastName}</p>
-      <button onClick={onRemove}>Remove</button>
+      <button onClick={()=> setDeleteOpen(true)}>Remove</button>
+      <Modal
+					className="delete-modal__container"
+					open={deleteOpen}
+					onClose={() => setDeleteOpen(false)}
+					slots={{backdrop: StyledBackdrop}}>
+						<div className='delete-modal'>
+							<h3> Are you sure you want to remove this friend?</h3>
+							<section className='delete-modal-buttons__container'>
+								<button onClick={onConfirmDelete}>Delete</button>
+								<button onClick={() => setDeleteOpen(false)}>Nevermind</button>
+							</section>
+
+						</div>	
+			</Modal>
     </div>
   )
 }
